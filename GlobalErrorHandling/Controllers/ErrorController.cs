76131d8df378ca63efe9b387d2b617a3ef1569e3
Lokendra Kumar <Lokendra_Kumar@Dellteam.com>
2020-09-15 18:07:49 +0530

    Code

---
+++ GlobalErrorHandling/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("error")] 
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var statusCode = exception.Error.GetType().Name switch
+            {
+                "ArgumentException" => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.ServiceUnavailable
+            };
+
+            return Problem(detail:exception.Error.Message, statusCode : (int) statusCode );
+        }
+    }
+}
---
+++ GlobalErrorHandling/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new ArgumentException("Error from test contoller");
+        }
+    }
+}
